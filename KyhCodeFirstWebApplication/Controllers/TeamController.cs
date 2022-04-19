97acd98bcf098d2606a1c2ae9d96c99b743016b6
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-19 10:45:31 +0200

    a

---
+++ KyhCodeFirstWebApplication/Controllers/TeamController.cs
@@ -0,0 +1,49 @@
+﻿using KyhCodeFirstWebApplication.Data;
+using KyhCodeFirstWebApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KyhCodeFirstWebApplication.Controllers;
+
+
+public class TeamController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public TeamController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    /*
+            - skriver vi kod som "KONTROLLERAR FLÖDET" vad som ska hända
+            - hämta från databas osv
+            - validera
+            - lägg properties i "Model"
+
+     */
+    public IActionResult Index()
+    {
+        var model = new TeamIndexViewModel
+        {
+            Items = _context.Teams.Select(e => new TeamIndexViewModel.TeamViewModel
+            {
+                Id = e.Id,
+                Name = e.Name
+            }).ToList()
+        };
+
+        //Delegera till en View
+        return View();
+
+    }
+    //public IActionResult New()
+    //{
+
+    //}
+    //public IActionResult Edit()
+    //{
+
+    //}
+
+
+}
---
+++ KyhCodeFirstWebApplication/ViewModels/TeamIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KyhCodeFirstWebApplication.ViewModels;
+
+public class TeamIndexViewModel
+{
+    public class TeamViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public List<TeamViewModel> Items { get; set; }
+}
