12c6bbd3ee1d83ec9fba0afd8bf88d823e70cef8
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-19 09:38:59 +0200

    fff

--- KyhCodeFirstWebApplication/Data/Team.cs
+++ KyhCodeFirstWebApplication/Data/Team.cs
@@ -1,9 +1,15 @@
-﻿namespace KyhCodeFirstWebApplication.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KyhCodeFirstWebApplication.Data;
 
 public class Team
 {
     public int Id { get; set; }
+
+    [MaxLength(100)]
     public string Name { get; set; }
+
+    [MaxLength(50)]
     public string City { get; set; }
     public int FoundedYear { get; set; }
 }
