b6a654783697a8aedd3ccd492aa0d414e43361b9
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-21 08:16:02 +0200

    registrationservice

---
+++ KyhCodeFirstWebApplication/Services/IRegistrationService.cs
@@ -0,0 +1,22 @@
+﻿namespace KyhCodeFirstWebApplication.Services;
+
+public interface IRegistrationService
+{
+    // Register
+    //Rules - ska misslyckas sig ifall man redan finns
+    //          ska lyckas ifall man inte finns
+    //      ska INTE skicka email om man finns
+    //      ska skicka email om man finns
+    //      vi tillåter bara Oslo som stad i Norge 
+
+    public enum RegistrationStatus
+    {
+        Ok,
+        AlreadyExists,
+        NotAllowedCityCountryCombination
+    }
+
+    RegistrationStatus Register(string email, string name, string city, string country);
+
+
+}
---
+++ KyhCodeFirstWebApplication/Services/RegistrationService.cs
@@ -0,0 +1,9 @@
+﻿namespace KyhCodeFirstWebApplication.Services;
+
+public class RegistrationService : IRegistrationService
+{
+    public IRegistrationService.RegistrationStatus Register(string email, string name, string city, string country)
+    {
+        return IRegistrationService.RegistrationStatus.Ok;
+    }
+}
