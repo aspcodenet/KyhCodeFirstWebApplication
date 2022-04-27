43562b0196441c81918b233913cdf0fc53e9a8ba
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-27 14:27:40 +0200

    aaa

---
+++ KyhCodeFirstWebApplication/Services/IMailingListService.cs
@@ -0,0 +1,32 @@
+﻿using KyhCodeFirstWebApplication.Data;
+
+namespace KyhCodeFirstWebApplication.Services;
+
+public interface IMailingListService
+{
+    void SendEmail(string email, string header, string mailbody );
+}
+
+public class  MailingListService : IMailingListService
+{
+    private readonly ApplicationDbContext _context;
+
+    public MailingListService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public void SendEmail(string email, string header, string mailbody)
+    {
+        //Skciakr mail
+
+        // Skapa en logevent för denna user
+        var user = _context.MailingListUsers.First(e => e.Email == email);
+        user.Events.Add(new LogEvent
+        {
+            Action = "mail sent", // "created", ""
+            Created = DateTime.Now
+        });
+        _context.SaveChanges();
+    }
+}
+
---
+++ TestProject1/Services/MailingListServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using KyhCodeFirstWebApplication.Data;
+using KyhCodeFirstWebApplication.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject1.Services;
+
+[TestClass]
+public class MailingListServiceTests
+{
+    private readonly MailingListService _sut;
+    private ApplicationDbContext _context;
+
+    public MailingListServiceTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "Test")
+            .Options;
+        _context = new ApplicationDbContext(options);
+        _sut = new MailingListService(_context);
+    }
+
+    [TestMethod]
+    public void When_sending_mail_a_log_event_should_be_created()
+    {
+        // Arrange
+
+        _context.MailingListUsers.Add(new MailingListUser
+        {
+            City = "321321",
+            Country = "", Email = "stefan@hej.se", Name = "Stefan"
+        });
+        _context.SaveChanges();
+
+
+
+        // Act
+        _sut.SendEmail("stefan@hej.se", "Hej", "nytt mailutckick bla bla");
+
+
+
+        //Assert
+        var user = _context.MailingListUsers.Include(e=>e.Events)
+            .First(e => e.Email == "stefan@hej.se");
+
+        var ev = user.Events.Last();
+
+        Assert.AreEqual("mail sent", ev.Action);
+
+    }
+}
