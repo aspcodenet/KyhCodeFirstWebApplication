3d3e01b4433d770912c4a78399dbe57c0d4717e4
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-04-27 14:03:12 +0200

    aaaa

--- KyhCodeFirstWebApplication/Data/MailingListUser.cs
+++ KyhCodeFirstWebApplication/Data/MailingListUser.cs
@@ -8,4 +8,13 @@ public class MailingListUser
     public string City { get; set; }
     public string Country { get; set; }
 
+    public List<LogEvent> Events { get; set; }
+
+}
+
+public class LogEvent
+{
+    public int Id { get; set; }
+    public DateTime Created { get; set; }
+    public string Action { get; set; }
 }
---
+++ KyhCodeFirstWebApplication/Migrations/20220427120158_log.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KyhCodeFirstWebApplication.Migrations
+{
+    public partial class log : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LogEvent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MailingListUserId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogEvent", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LogEvent_MailingListUsers_MailingListUserId",
+                        column: x => x.MailingListUserId,
+                        principalTable: "MailingListUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LogEvent_MailingListUserId",
+                table: "LogEvent",
+                column: "MailingListUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogEvent");
+        }
+    }
+}
