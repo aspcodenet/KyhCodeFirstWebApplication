979f39255892669eb6cd86397fba025717da0167
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-05-05 08:59:33 +0200

    aaaa

---
+++ KyhCodeFirstWebApplication/Controllers/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using KyhCodeFirstWebApplication.Data;
+using KyhCodeFirstWebApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KyhCodeFirstWebApplication.Controllers;
+
+public class PlayerController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public PlayerController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    // GET
+    public IActionResult Index()
+    {
+        //1. Hämta data från databas
+        //2. populera en "snäll" vymodell med exakta properties som ska ritas ut
+        var model = new PlayerIndexViewModel();
+        model.Items = _context.Players.Select(e => new PlayerIndexViewModel.PlayerIndexItemViewModel
+        {
+            Name = e.Name,
+            Id = e.Id
+        }).ToList();
+
+        //3. Anropa View
+        return View(model);
+    }
+}
---
+++ KyhCodeFirstWebApplication/ViewModels/PlayerIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace KyhCodeFirstWebApplication.ViewModels;
+
+public class PlayerIndexViewModel
+{
+    public List<PlayerIndexItemViewModel> Items { get; set; }
+
+    public class PlayerIndexItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
